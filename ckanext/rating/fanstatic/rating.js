c42da346c92150b0e873fa1540c72cb745a5095f
Mikel Emaldi <m.emaldi@gmail.com>
2015-09-24 10:16:07 +0200

    Fixed a bug which avoided the proper rating of datasets when many of them where displayed together

--- ckanext/rating/fanstatic/rating.js
+++ ckanext/rating/fanstatic/rating.js
@@ -1,4 +1,11 @@
-$("#rating-widget").bind('rated', function (event, value) {
+$(".rateit").bind('rated', function (event, value) {
+  var eventID = event.currentTarget.id;
+  var packageID = eventID.replace("rating-widget-", "");
+  $.post("/api/3/action/rating_create", JSON.stringify({"package": packageID, "rating": value}), 'json')
+});
+
+/*$("#rating-widget").bind('rated', function (event, value) {
+  console.log(event);
   var packageId = $("#package-id")[0].value;
   $.post("/api/3/action/rating_create", JSON.stringify({"package": packageId, "rating": value}), 'json')
-});
+});*/
